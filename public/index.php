a7c42225d74b1e4acff7419a9f902f676fda792d
Saurav <sk2808564@gmail.com>
2025-12-14 00:37:35 +0530

    changes

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+// Development-only: show all errors & warnings
+ini_set('display_startup_errors', '1');
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/Controller.php';
+require_once __DIR__ . '/../src/View.php';
+
+
+
+?>
